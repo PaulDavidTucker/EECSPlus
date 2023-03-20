9cbc2f40f1ffe4edd81b55d3868653c5449a084f
Paul Tucker <tuckerpauldavid@gmail.com>
2023-03-20 17:26:48 +0000

    help

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,1 @@
-let myIssue = new Issue();
-myIssue.IssueDescription = "This is a test issue";
-
-//This should not work
-print(myIssue.IssueDescription);
-
-//This should print "Hello World!"
-console.log("Hello World!")
+//Issues with creating Objects from Classes.js here
