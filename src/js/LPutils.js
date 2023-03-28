36c05b92ee84eaec5fde1f88b45188bd2d208551
Paul Tucker <tuckerpauldavid@gmail.com>
2023-03-28 14:54:24 +0100

    Dynamically checks to see preferred theme

--- src/js/LPutils.js
+++ src/js/LPutils.js
@@ -1,11 +1,35 @@
 var isDarkMode = false;
-
 const checkbox = document.getElementById('darkModeCheckBox');
 const doc = document.getElementById("html")
 const navbar = document.getElementById("nav")
 const jumbotron = document.getElementById("jumbotron")
 const LogoutButton = document.getElementById("LogoutButton")
+const storedTheme = localStorage.getItem('theme')
 
+const getPreferredTheme = () => {
+  if (storedTheme) {
+    return storedTheme
+  }
+
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+}
+
+const setTheme = function (theme) {
+  if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    doc.setAttribute("data-bs-theme", "dark");
+    navbar.setAttribute("class", "navbar navbar-expand-lg navbar-dark bg-dark");
+    jumbotron.setAttribute("style", "background:rgb(53, 53, 53) !important");
+    LogoutButton.setAttribute("class", "btn btn-outline-light");
+    isDarkMode = true;
+  } else {
+    doc.setAttribute("data-bs-theme", theme);
+    navbar.setAttribute("class", "navbar navbar-expand-lg navbar-light gradient-custom-navbar");
+    jumbotron.setAttribute("style", "");
+    LogoutButton.setAttribute("class", "btn btn-outline-dark");
+  }
+}
+
+setTheme(getPreferredTheme())
 
 checkbox.addEventListener('change', ()=>{
     if (isDarkMode == false) {
