c09f4e1ba5071c516478bcb9da5f0c9f5437bdc7
Paul Tucker <tuckerpauldavid@gmail.com>
2023-03-28 14:32:20 +0100

    NavBar colour changes with DM

--- src/js/LPutils.js
+++ src/js/LPutils.js
@@ -2,15 +2,18 @@ var isDarkMode = false;
 
 const checkbox = document.getElementById('darkModeCheckBox');
 const doc = document.getElementById("html")
-console.log(doc);
+const navbar = document.getElementById("nav")
+
 
 checkbox.addEventListener('change', ()=>{
     if (isDarkMode == false) {
         isDarkMode = true;
         doc.setAttribute("data-bs-theme", "dark");
+        navbar.setAttribute("class", "navbar navbar-expand-lg navbar-dark bg-dark");
     } else {
         isDarkMode = false;
         doc.setAttribute("data-bs-theme", "light");
+        navbar.setAttribute("class", "navbar navbar-expand-lg navbar-light gradient-custom-navbar");
     } 
     })
 
