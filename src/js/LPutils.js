577d1be9ab72929ff7f2c7374375c4268d305834
Paul Tucker <tuckerpauldavid@gmail.com>
2023-03-28 14:40:05 +0100

    I am actually a god

--- src/js/LPutils.js
+++ src/js/LPutils.js
@@ -3,6 +3,7 @@ var isDarkMode = false;
 const checkbox = document.getElementById('darkModeCheckBox');
 const doc = document.getElementById("html")
 const navbar = document.getElementById("nav")
+const jumbotron = document.getElementById("jumbotron")
 
 
 checkbox.addEventListener('change', ()=>{
@@ -10,10 +11,12 @@ checkbox.addEventListener('change', ()=>{
         isDarkMode = true;
         doc.setAttribute("data-bs-theme", "dark");
         navbar.setAttribute("class", "navbar navbar-expand-lg navbar-dark bg-dark");
+        jumbotron.setAttribute("style", "background:rgb(53, 53, 53) !important");
     } else {
         isDarkMode = false;
         doc.setAttribute("data-bs-theme", "light");
         navbar.setAttribute("class", "navbar navbar-expand-lg navbar-light gradient-custom-navbar");
+        jumbotron.setAttribute("style", "");
     } 
     })
 
