3d795346fa8c1b94c63f4a3740c7f8fd1c784250
abi <a.ravindran@se21.qmul.ac.uk>
2023-04-11 20:00:55 +0100

    admin update

--- src/pages/adminLanding.php
+++ src/pages/adminLanding.php
@@ -207,7 +207,7 @@ $conn -> close();
                             <tbody>
                                 <tr>
                                     <th scope="row">1</th>
-                                    <td>Abishana Ravindran</td>
+                                    <td>John Doe</td>
                                     <td>21038475</td>
                                     <td>Pending</td>
                                 </tr>
@@ -227,6 +227,13 @@ $conn -> close();
                                     <th scope="row">4</th>
                                     <td>Person 4</td>
                                     <td>234345</td>
+                                    <td>Pending</td>
+                                </tr>
+                                </tr>
+                                <tr>
+                                    <th scope="row">4</th>
+                                    <td>Person 5</td>
+                                    <td>234845</td>
                                     <td>Pending</td>
                                 </tr>
                                 </tr>
