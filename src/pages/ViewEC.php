9460ddda0e0e645dab2c620e7810b4589533d9c1
mchoud2 <mustaqeemrchoudhury@gmail.com>
2023-04-12 23:48:13 +0100

    Stable release (backup)

--- src/pages/ViewEC.php
+++ src/pages/ViewEC.php
@@ -38,7 +38,72 @@ elseif ($_SESSION['user_type'] == 'Admin'  ){
 <body>
 
     <!--NavBar for top of page-->
-    <?php include 'topNav.php';?>
+    <nav class="navbar navbar-expand-lg navbar-light gradient-custom-navbar" id="nav">
+        <a class="navbar-brand ml-1" href="../index.php">
+            <img src="../assets/QMUL Logo.png" alt="Logo not found" width="30" height="30" class="d-inline-block align-text-top">
+            EECSPlus 
+        </a>
+        <a class="ml-auto mr-1" href="../logout.php">
+          <button type="button" id="LogoutButton" class="btn btn-outline-dark ">
+            <img src="../assets/icons/person.svg" alt="Logout">
+            Logout
+        </button>
+        </a>
+        
+            <button class="navbar-toggler mr-1" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item active ml-1">
+                <a class="nav-link" href="LandingPage.php">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item dropdown ml-1">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Submit
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <?php
+                      if ($userType !== 'Faculty') {
+                        echo '<a class="dropdown-item" href="ApplyEC.php">Submit ECs</a>';
+                      }
+                      ?>
+                      <a class="dropdown-item" href="ReportIssues.php">Submit Issue</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown ml-1">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    View
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <a class="dropdown-item" href="EECSServices.php">View EECS Services</a>
+                      <div class="dropdown-divider"></div>
+                    <?php
+                      if ($userType !== 'Faculty') {
+                        echo '<a class="dropdown-item" href="ViewEC.php">View your ECs</a>';
+                      }
+                      ?>
+                      <a class="dropdown-item" href="ViewYourIssues.php">View your issues</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="ViewAllIssues.php">View all issues</a>
+                    </div>
+                </li>
+                <li class="nav-item ml-1 mt-2">
+                  <div class="form-switch">
+                    <input class="form-check-input" type="checkbox" role="switch" id="darkModeCheckBox"  checked="true"  >
+                    <label class="form-check-label" for="darkModeCheckBox">Dark Mode</label>
+                  </div>
+                </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0 ml-1 mr-1">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        
+        
+    </nav>
 
     <div class="container-fluid text-center">
       <h1>View your ECs</h1>
