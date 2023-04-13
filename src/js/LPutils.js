cc3c4362cb1228732357f4a3c9e703e8cc733590
Paul Tucker <tuckerpauldavid@gmail.com>
2023-04-13 14:03:23 +0100

    Global Darkmode var finished

--- src/js/LPutils.js
+++ src/js/LPutils.js
@@ -1,4 +1,5 @@
 var isDarkMode = false;
+var GlobalTheme = "";
 const checkbox = document.getElementById('darkModeCheckBox');
 const doc = document.getElementById("html")
 const navbar = document.getElementById("nav")
@@ -6,8 +7,21 @@ const jumbotron = document.getElementById("jumbotron")
 const LogoutButton = document.getElementById("LogoutButton")
 const storedTheme = localStorage.getItem('theme')
 
+/**
+ * 
+ * @param {*} param - Takes a string that will change the value of the GlobalTheme variable. This will be put in local storage, and will be used to set the theme on page load.
+ */
+function updateStoredTheme(param) {
+  localStorage.setItem('theme', param);
+}
 
+
+/**
+ * This function enables dark mode. It changes the data-bs-theme attribute on the html element to dark, and changes the navbar to a dark theme. 
+ */
 function EnableDarkMode() {
+  updateStoredTheme("dark");
+  console.log("Theme changed to: "+localStorage.getItem('theme'));
   isDarkMode = true;
   doc.setAttribute("data-bs-theme", "dark");
   navbar.setAttribute("class", "navbar navbar-expand-lg navbar-dark bg-dark");
@@ -20,9 +34,18 @@ function EnableDarkMode() {
   }
   
   LogoutButton.setAttribute("class", "btn btn-outline-light");
+  checkbox.setAttribute("checked", "true");
 }
 
+/**
+ * 
+ * @param {*} theme - The theme to be set. This is used to set the data-bs-theme attribute on the html element.
+ *  
+ */
 function EnableLightMode(theme) {
+  updateStoredTheme("light");
+  console.log("Theme changed to: "+localStorage.getItem('theme'));
+  isDarkMode = false;
   doc.setAttribute("data-bs-theme", theme);
   navbar.setAttribute("class", "navbar navbar-expand-lg navbar-light gradient-custom-navbar");
   try {
@@ -32,20 +55,31 @@ function EnableLightMode(theme) {
   }
   
   LogoutButton.setAttribute("class", "btn btn-outline-dark");
+  checkbox.setAttribute("checked", "false");
 }
 
 const getPreferredTheme = () => {
-  if (storedTheme) {
-    return storedTheme
+  GlobalTheme = localStorage.getItem('theme');
+  if (GlobalTheme !== "") {
+    console.log("GlobalTheme is not empty, returning GlobalTheme");
+    return GlobalTheme
   }
+  console.log(GlobalTheme)
 
-  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+  updateStoredTheme(window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');  
+  return GlobalTheme
+  
 }
 
 const setTheme = function (theme) {
   if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    EnableDarkMode();
+    console.log("Dark Mode Enabled by default");
+    checkbox.setAttribute("checked", "true");
+    isDarkMode = true;
+  } else if (theme === 'light'){
     EnableLightMode(theme);
-    console.log("Light Mode disabled by default");
+    console.log("Light Mode enabled by default");
     isDarkMode = false;
   } else {
     EnableDarkMode();
@@ -55,38 +89,18 @@ const setTheme = function (theme) {
   }
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+  setTheme(getPreferredTheme());
+  console.log("Theme set to: " + localStorage.getItem('theme') + " by default");
+}, false);
 
 
-setTheme(getPreferredTheme())
 
 checkbox.addEventListener('change', ()=>{
     if (isDarkMode == false) {
-        isDarkMode = true;
-        doc.setAttribute("data-bs-theme", "dark");
-        navbar.setAttribute("class", "navbar navbar-expand-lg navbar-dark bg-dark");
-        //Add any bootstrap elements that are < v5, and need to be changed to dark mode. The style attribute is used to override the default bootstrap theme.
-        //These are the only elements that need to be changed to dark mode, as the rest of the bootstrap elements are changed by the data-bs-theme attribute.
-        try {
-          jumbotron.setAttribute("style", "background:rgb(53, 53, 53) !important");
-        } catch (error) {
-          console.log("Jumbotron not found");
-        }
-        
-        LogoutButton.setAttribute("class", "btn btn-outline-light");
-        console.log("Dark Mode Enabled");
-        checkbox.setAttribute("checked", "true");
+        EnableDarkMode();
     } else {
-        isDarkMode = false;
-        doc.setAttribute("data-bs-theme", "light");
-        navbar.setAttribute("class", "navbar navbar-expand-lg navbar-light gradient-custom-navbar");
-        try {
-          jumbotron.setAttribute("style", "");
-        } catch (error) { 
-          console.log("Jumbotron not found");
-        }
-        LogoutButton.setAttribute("class", "btn btn-outline-dark");
-        console.log("Dark Mode Disabled");
-        checkbox.setAttribute("checked", "false");
+        EnableLightMode("light");
     } 
     })
 
